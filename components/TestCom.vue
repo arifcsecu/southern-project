bd7179d4353df45130932d8f257ea63b25ed69d8
Ariful Islam <arifcsecu16@gmail.com>
2025-05-06 12:38:45 +0600

    test

---
+++ components/TestCom.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <PopoverButton
+      class="inline-flex items-center justify-center rounded-xl p-2 text-[#426058] bg-gray-100 hover:bg-gray-200 focus:outline-none"
+    >
+      <span class="sr-only">Open menu</span>
+      <Icon name="zondicons:menu" class="h-5 w-5 md:h-8 md:w-8"></Icon>
+    </PopoverButton>
+  </div>
+</template>
+
+<script setup>
+
+import {
+  Popover,
+  PopoverButton,
+  PopoverGroup,
+  PopoverPanel,
+} from "@headlessui/vue";
+</script>
--- pages/about.vue
+++ pages/about.vue
@@ -3,4 +3,11 @@
     <h2 class="text-2xl font-semibold text-green-600 mb-2">About Page</h2>
     <p class="text-gray-700">This is the About page of your Nuxt site.</p>
   </div>
+  <TestCom/>
 </template>
+
+<script setup>
+
+import TestCom from "~/components/TestCom.vue";
+
+</script>
